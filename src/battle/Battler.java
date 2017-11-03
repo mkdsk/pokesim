a704deee52fc0034b4f74d274125d37ad275e817
mkdsk <shredda763@gmail.com>
2017-11-03 08:47:55 -0400

    New code

--- src/battle/Battler.java
+++ src/battle/Battler.java
@@ -18,12 +18,25 @@ public class Battler {
      */
     public void battle(File file){
         battling = true;
+        Game.getTextHelper().print("Battling " + file.getName());
         while(battling){
             /* MAIN BATTLE LOOP */
-            //get our pokemon to battle with
-            //get base hp of each pokemon
-            int opponentHP;
-            int myHP;
+            File enemy = file;
+            File player = Game.getPartyPokemon();
+            if(player == null){
+                battling = false;
+                Game.getTextHelper().print("Error: You need a party Pokemon to battle.");
+                Game.gameLoop(true);
+            }
+            //Check if our attacks and enemy attacks are valid.
+            int p_Atk = Game.getFileManager().getAtk(player);
+            int p_Def = Game.getFileManager().getDef(player);
+            
+            
+            
+            //Get the stats of our Pkmn
+            
+            //Get the stats of enemy Pkmn
         }
     }
 
